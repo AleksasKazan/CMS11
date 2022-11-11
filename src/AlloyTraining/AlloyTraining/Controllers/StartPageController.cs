0af2678e16e5d53254d517552872a19fb51b110e
Aleksas Kazanavicius <aleksas.kazanavicius@unrvld.com>
2022-11-11 12:57:05 +0200

    localization added

--- src/AlloyTraining/AlloyTraining/Controllers/StartPageController.cs
+++ src/AlloyTraining/AlloyTraining/Controllers/StartPageController.cs
@@ -1,11 +1,6 @@
-﻿using EPiServer;
-using EPiServer.Core;
-using EPiServer.Framework.DataAnnotations;
+﻿using AlloyTraining.Models.Pages;
 using EPiServer.Web.Mvc;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web.Mvc;
-using AlloyTraining.Models.Pages;
 
 namespace AlloyTraining.Controllers
 {
@@ -13,9 +8,6 @@ namespace AlloyTraining.Controllers
     {
         public ActionResult Index(StartPage currentPage)
         {
-            /* Implementation of action. You can create your own view model class that you pass to the view or
-             * you can pass the page type for simpler templates */
-
             return View(currentPage);
         }
     }
--- src/AlloyTraining/AlloyTraining/Models/Pages/StartPage.cs
+++ src/AlloyTraining/AlloyTraining/Models/Pages/StartPage.cs
@@ -6,22 +6,20 @@ using System.ComponentModel.DataAnnotations;
 namespace AlloyTraining.Models.Pages
 {
     [ContentType(DisplayName = "Start",
-        GUID = "95902579-ef6b-4bc1-aa5f-dac739be354d",
+        // your code will have a random GUID here
         GroupName = SiteGroupNames.Specialized, Order = 10,
         Description = "The home page for a website with an area for blocks and partial pages.")]
     public class StartPage : PageData
     {
         [CultureSpecific]
-        [Display(
-            Name = "Heading",
+        [Display(Name = "Heading",
             Description = "If the Heading is not set, the page falls back to showing the Name.",
             GroupName = SystemTabNames.Content, Order = 10)]
         public virtual string Heading { get; set; }
 
         [CultureSpecific]
-        [Display(
-            Name = "Main body",
-            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+        [Display(Name = "Main body",
+            Description = "The main body uses the XHTML-editor you can insert for example text, images, and tables.",
             GroupName = SystemTabNames.Content, Order = 20)]
         public virtual XhtmlString MainBody { get; set; }
 
