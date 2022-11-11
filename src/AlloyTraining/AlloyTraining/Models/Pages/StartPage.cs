cb19a6e4ab850cb8dc06030253316a262adac491
Aleksas Kazanavicius <aleksas.kazanavicius@unrvld.com>
2022-11-11 12:44:30 +0200

    start page created

---
+++ src/AlloyTraining/AlloyTraining/Models/Pages/StartPage.cs
@@ -0,0 +1,33 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "Start",
+        GUID = "95902579-ef6b-4bc1-aa5f-dac739be354d",
+        GroupName = SiteGroupNames.Specialized, Order = 10,
+        Description = "The home page for a website with an area for blocks and partial pages.")]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Heading",
+            Description = "If the Heading is not set, the page falls back to showing the Name.",
+            GroupName = SystemTabNames.Content, Order = 10)]
+        public virtual string Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content, Order = 20)]
+        public virtual XhtmlString MainBody { get; set; }
+
+        [Display(Name = "Main content area",
+            Description = "The main content area contains an ordered collection to content references, for example blocks, media assets, and pages.",
+            GroupName = SystemTabNames.Content, Order = 30)]
+        public virtual ContentArea MainContentArea { get; set; }
+    }
+}
